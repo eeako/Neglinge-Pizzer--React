739dafde6a46b6df35d95c48e1eaf8c3f074cbbd
eeako <125037694+eeako@users.noreply.github.com>
2023-03-21 10:17:04 +0100

    Add files via upload

---
+++ mainPages/Grill.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Grill extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <div id="specMenuGrid-container">
+                    <h1>GRILL</h1>
+                </div>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+                <script src="script.js"></script>
+            </div>
+        )
+    }
+}
+
+export default Grill;
